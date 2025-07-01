f7200533eb2947d815a4bbc9c7440f711fa8b47f
elodiecartier91 <41297439-elodiecartier91@users.noreply.replit.com>
2025-07-01 01:05:31 +0000

    Initial commit

---
+++ attached_assets/index_1751331167312.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SafeDashboard from "./SafeDashboard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SafeDashboard />);
